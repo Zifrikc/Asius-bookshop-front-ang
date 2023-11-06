cf942cc7a2fef17092517f968ddde795a314e172
Stil Reniun <ekatndcp@gmail.com>
2023-11-06 02:45:56 +0100

    Ultimos cambios hechos

--- src/app/Pages/Users/users-perfil/users-perfil.component.ts
+++ src/app/Pages/Users/users-perfil/users-perfil.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/Services/login.service';
 
 @Component({
   selector: 'app-users-perfil',
   templateUrl: './users-perfil.component.html',
   styleUrls: ['./users-perfil.component.css']
 })
-export class UsersPerfilComponent {
+export class UsersPerfilComponent implements OnInit {
+  user:any=null
+
+  constructor( private login:LoginService) { }
+
+  ngOnInit(): void {
+    this.user = this.login.getuser();
+  }
 
 }
